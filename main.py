1f971585732b5a08cb940d3526dbc968b0364130
Arina <105871709+AriSky1@users.noreply.github.com>
2023-06-07 19:55:26 +0200

    'msg'

--- main.py
+++ main.py
@@ -1,25 +1,59 @@
 from dash import Dash, dcc, html, Input, Output, State
+from flask import Flask, render_template, Response
+import dash_bootstrap_components as dbc
 
-app = Dash(__name__)
 
-app.layout = html.Div([
-    html.Div(dcc.Input(id='input-on-submit', type='text')),
-    html.Button('Submit', id='submit-val', n_clicks=0),
-    html.Div(id='container-button-basic',
-             children='Enter a value and press submit')
-])
+external_stylesheets = [
+    'https://fonts.googleapis.com/css2?family=Wix+Madefor+Display:wght@600&display=swap',
+     dbc.themes.CYBORG
+]
+
+style_title={'color': 'grey','fontSize': 30,'textAlign': 'center', 'letter-spacing':'2px', 'padding-left': '20px','padding-top': '20px'}
+style_text={'color': 'grey','fontSize': 18,'textAlign': 'center','font_family': 'Segoe UI', 'padding-bottom':'20px','padding-left': '20px'}
+style_btn = {'color': 'black','font-weight': 'bold', 'width':'200px', 'height':'100px',}
+
+server = Flask(__name__)
+app = Dash(__name__, server=server,external_stylesheets=external_stylesheets)
+
+app.layout = html.Div(
+    [
+        dbc.Container(
+            [
+                html.Div(
+                    [
+                        html.H1(children='Objects Recognizer', style=style_title),
+                        html.Div(
+                            children='Recognize multiple objects from your live web cam stream.',
+                            style=style_text
+                        ),
+                        html.Button('Start', id='start_btn', n_clicks=0, className='btn btn-success'),
+                        html.Div(id='container-stream')
+                    ],
+                    className='my-5 text-center'
+                )
+            ],
+            fluid=True
+        )
+    ],
+    style={"display": "flex", "flex-direction": "column", "align-items": "center"}
+)
 
 
 @app.callback(
-    Output('container-button-basic', 'children'),
-    Input('submit-val', 'n_clicks'),
-    State('input-on-submit', 'value')
+    Output('container-stream', 'children'),
+    Input('start_btn', 'n_clicks')
+
 )
-def update_output(n_clicks, value):
-    return 'The input value was "{}" and the button has been clicked {} times'.format(
-        value,
-        n_clicks
-    )
+
+
+def load_stream(n_clicks):
+    if n_clicks > 0:
+        # Your code to start the object recognition stream goes here
+        # Replace the following line with your implementation
+        return html.Div("Object recognition stream has started.")
+    else:
+        return html.Div()
+
 
 
 if __name__ == '__main__':
